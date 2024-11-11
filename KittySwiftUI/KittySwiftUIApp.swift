7ee76d64a0ae2817bfc664d26bf95bf78763982b
Hoang Dinh <hoang.dinh@s3corp.com.vn>
2024-11-11 09:35:39 +0700

    first commit

---
+++ KittySwiftUI/KittySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  KittySwiftUIApp.swift
+//  KittySwiftUI
+//
+//  Created by hoang.dinh on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct KittySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
